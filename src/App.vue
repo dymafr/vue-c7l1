5f359a91b8c8fee29101cd6c1c298b9d2981732c
dymafr <erwan1309@hotmail.com>
2022-03-01 11:56:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 v-memo="[title]">
+    <p>{{ title }}</p>
+  </h1>
+  <input type="text" v-model="input" />
+  <p>{{ input }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref('Je suis un titre');
+const input = ref('');
+</script>
+
+<style scoped lang="scss"></style>
